b88e0adc15260614911d3cc24636a8d48e57100e
Eliana Villafranca <villafranca.eliana@gmail.com>
2019-11-25 19:17:26 -0500

    twitter api

---
+++ backend/backend/main/twitter_api.py
@@ -0,0 +1,17 @@
+import twitter
+from . import settings
+
+TWEET_COUNT = 200
+
+def create_conn():
+	twt = settings.TWITTER_API_ACCESS
+	api = twitter.Api(consumer_key = twt['TWT_API_KEY'],
+	                  consumer_secret = twt['TWT_API_SECRET'],
+	                  access_token_key = twt['TWT_ACCESS_TOKEN'],
+	                  access_token_secret= twt['TWT_ACCESS_SECRET'],
+	)
+	return api
+
+def get_tweets(hashtag, time_period):
+  count = TWEET_COUNT
+  api.GetSearch(raw_query=f'q=%23{hashtag}&result_type=recent&since={time_period}&count={count}')
